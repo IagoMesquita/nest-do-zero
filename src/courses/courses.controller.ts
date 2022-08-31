0557c937698eb6eab7701f6af069d28214ae08f3
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-31 12:45:46 -0300

    feat: Params request

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
+// @Controller()
 @Controller('courses')
 export class CoursesController {
+  // @Get('courses')
   @Get()
   findAll() {
     return 'Lista de Cursos';
+  }
+
+  @Get(':id')
+  // findOne(@Param() params) {
+  //   return `Curso #${params.id}`;
+  // }
+  // desconstruindo:
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
   }
 }
