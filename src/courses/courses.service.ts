0dad88ccf68b531eabdb41d6af1234fa3996a7c6
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-31 19:15:41 -0300

    feat: method update and remore in service

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -23,4 +23,21 @@ export class CoursesService {
   create(createCourseDto: any) {
     this.courses.push(createCourseDto);
   }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
 }
