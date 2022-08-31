b24876dab1d797de1d2d0eaa6de1618feee374b7
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-31 14:04:23 -0300

    feat: customizing Statuscode

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 // @Controller()
 @Controller('courses')
@@ -7,6 +16,12 @@ export class CoursesController {
   @Get()
   findAll() {
     return 'Lista de Cursos';
+  }
+
+  /*Aqui usamos um Status personalizado, bem parecido com o que fazemos no Express. Usando o @Res*/
+  @Get('listagem')
+  findLis(@Res() response) {
+    return response.status(200).send('Lista de produtos com @Res');
   }
 
   @Get(':id')
@@ -19,6 +34,9 @@ export class CoursesController {
   }
 
   @Post()
+  /* passa um status que não é default, podemos passar o numero ou usar o HttpStatus 
+  util qando código de status é estático*/
+  @HttpCode(HttpStatus.NO_CONTENT)
   // para pegar algo especifoco, descontruo oq quero pegar
   create(@Body('price') body) {
     return body;
