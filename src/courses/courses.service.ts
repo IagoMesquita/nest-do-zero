dc266c2939392d32e55f8a8fe7b0d346d2ed31fa
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-31 14:45:02 -0300

    feat: class CousesService with atribute and method findAll and FindOne

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      description: 'Um framework Node.js para o back-and',
+      tags: ['node.js', 'nest.js', 'javascript'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    return this.courses.find((course: Course) => course.id === Number(id));
+  }
+}
