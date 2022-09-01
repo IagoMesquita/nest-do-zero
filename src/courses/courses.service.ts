916e140f91f13795fdd11ad274b86b755d24dead
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-01 15:53:38 -0300

    feat: deal with errors

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 
 @Injectable()
@@ -17,7 +17,19 @@ export class CoursesService {
   }
 
   findOne(id: string) {
-    return this.courses.find((course: Course) => course.id === Number(id));
+    const course = this.courses.find(
+      (course: Course) => course.id === Number(id),
+    );
+
+    if (!course) {
+      // throw new HttpException(`Course ID ${id} not found`, 404);
+      throw new HttpException(
+        `Course ID ${id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    return course;
   }
 
   create(createCourseDto: any) {
@@ -41,3 +53,11 @@ export class CoursesService {
     }
   }
 }
+
+/* 
+  Tratando Exceções:
+  usar " throw new HttpException" que recebe dois parametros.
+  A mensagem a ser exibida e o status, que pode ser direto o numero ou
+  ou os metodos que vem no Nest
+     throw new HttpException(`Course ID ${id} not found`, HttpStatus.NOT_FOUND );
+*/
