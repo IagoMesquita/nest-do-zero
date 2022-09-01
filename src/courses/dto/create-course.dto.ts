cab1dd6ab00fe76bebc5673f9adc76cb80981d05
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-01 19:31:20 -0300

    feat: use class-validate in dto createCourse

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,7 +1,11 @@
+import { IsString } from 'class-validator';
+
 export class CreateCourseDto {
-  readonly id: number;
+  @IsString()
   readonly name: string;
+  @IsString()
   readonly description: string;
+  @IsString({ each: true })
   readonly tags: string[];
 }
 
