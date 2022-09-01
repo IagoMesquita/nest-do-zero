2c1b8d260a8561806c45e8b009ff5d55a82d6cae
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-01 20:11:45 -0300

    feat: transform

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,16 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(3000);
 }
 bootstrap();
+
+/*
+  whitelist: lista de pesmissões das propriedade que vem na req, são os dto's definidos
+  forbidNonWhitelisted: só permitem que venham os dados da req definidos na whitelist
+  transform: faz  com que os objetos que venham das rotas, nesse caso, create e update, 
+    sejam do tipo definido nos dto's.
+*/
