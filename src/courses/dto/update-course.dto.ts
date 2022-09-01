684e2d0b83e1df971bd798bceaeece55f1f1fe18
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-09-01 19:50:28 -0300

    feat: extends of PartialType

--- src/courses/dto/update-course.dto.ts
+++ src/courses/dto/update-course.dto.ts
@@ -1,5 +1,15 @@
-export class UpdateCourseDto {
-  readonly name?: string;
-  readonly description?: string;
-  readonly tags?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCourseDto } from './create-course.dto';
+
+export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
+
+// export class UpdateCourseDto {
+//   readonly name?: string;
+//   readonly description?: string;
+//   readonly tags?: string[];
+// }
+
+/*
+  Para não repetir o dto de Create e pelo fato do dto de Update
+  ser opcional, podemos usar o Patitial da biblioteca @nestjs/mapped-types
+*/
