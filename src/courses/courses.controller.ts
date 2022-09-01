10d1a53d86981a5058e645d40199ca3468ff6167
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-31 21:25:27 -0300

    feat: using methods service in controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -3,12 +3,9 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -17,29 +14,33 @@ export class CoursesController {
   constructor(private readonly courseService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).send('Lista de Cursos');
+  findAll() {
+    // return response.status(200).send('Lista de Cursos');
+    return this.courseService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Curso #${id}`;
+    // return `Curso #${id}`;
+    return this.courseService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body('price') body) {
-    return body;
+  create(@Body() body) {
+    // return body;
+    this.courseService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Atualização do curso #${id}`;
+    // return `Atualização do curso #${id}`;
+    return this.courseService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `Exclusão do Curso #${id}`;
+    // return `Exclusão do Curso #${id}`;
+    return this.courseService.remove(id);
   }
 }
 
